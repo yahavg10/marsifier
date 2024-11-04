98df77e1c87e49863945de28b29968776cee88c6
yahavg10 <yahavg10@gmail.com>
2024-11-04 11:14:59 +0200

    added strategy pool

--- src/receiver/data_sources_handlers/data_source_handler_template.py
+++ src/receiver/data_sources_handlers/data_source_handler_template.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
 
+from src.utils.pool import PoolFactory
 
-class DataSourceHandlerTemplate(ABC):
+
+class DataSourceHandler(ABC):
+    def __init__(self):
+        self.strategy_pool = PoolFactory.create_pool_strategy()
+
+    def get_strategy_pool(self):
+        return self.strategy_pool
+
     @abstractmethod
     def fetch_data(self):
         raise NotImplementedError
