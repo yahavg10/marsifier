5dad6e33288aca42176069c7b6db5f1848cf2797
yahavg10 <yahavg10@gmail.com>
2024-11-13 11:18:13 +0200

    Update sender.py

--- src/sender/sender.py
+++ src/sender/sender.py
@@ -31,6 +31,7 @@ def send(app_config: AppConfig, common_name: str, sender_type: str, payload_fn=f
     try:
         app_config.sender[sender_type]["params"]["common_name"] = common_name
         payload = payload_fn(**app_config.sender[sender_type]["params"])
+        print(f"the payload is {payload}")
         response = send_method(app_config.sender[sender_type]["endpoint"], files=payload)
         # response = send_method(app_config.sender[sender_type]["endpoint"], files=payload)
         logger.info(response)
